2ce70faa1c75d59f813f1d83cf8767f3ccfae01c
joacoEscobar <joaco.escobar.h@gmail.com>
2024-03-17 16:53:48 -0300

    entrega desafio 2

--- productManager.js
+++ productManager.js
@@ -1,19 +1,47 @@
+const fs = require('fs');
+
 
 class Productmanager {
     #products
+    #path;
     static ids = 0
-    constructor (){
-        this.#products = []
+    constructor() {
+        this.#path = './Data_products/products.json'
+        this.#products = this.#readProducts();
+    }
+    #assignId() {
+        let id = 1
+        if (this.#products.length != 0)
+            id = this.#products[this.#products.length - 1].id + 1;
+        return id
+    }
+    #readProducts() {
+        try {
+            if (fs.existsSync(this.#path)) {
+                return JSON.parse(fs.readFileSync(this.#path, 'utf-8'))
+            }
+            return [];
+        } catch (error) {
+            console.log(`ocurrio un error al leer el archivo, ${error}`);
+        }
+    }
+
+    #saveFile() {
+        try {
+            fs.writeFileSync(this.#path, JSON.stringify(this.#products))
+        } catch (error) {
+            console.log(`ocurrio un error al gurdar el archivo ${error}`);
+        }
     }
     addProduct(title, description, price, thumbnail, code, stock) {
         if (!title || !description || !price || !thumbnail || !code || !stock)
-        return 'Todos los datos son necesarios (title, description, price, thumbnail, code, stock)'
-     const codeRepeat = this.#products.some(p => p.code == code)
-     if (codeRepeat)
-        return `El codigo ${code} ya esta ocupado, intente nuevamente`
-        
-        Productmanager.ids = Productmanager.ids +1
-        const id = Productmanager.ids
+            return 'Todos los datos son necesarios (title, description, price, thumbnail, code, stock)'
+        const codeRepeat = this.#products.some(p => p.code == code)
+        if (codeRepeat)
+            return `El codigo ${code} ya esta ocupado, intente nuevamente`
+
+        Productmanager.ids = Productmanager.ids + 1
+        const id = this.#assignId()
         const newProduct = {
             id,
             title,
@@ -23,19 +51,41 @@ class Productmanager {
             code,
             stock
         };
-        this.#products.push(newProduct)
-        
-        return'Producto añaido con exito'
+        this.#products.push(newProduct);
+        this.#saveFile()
+
+        return 'Producto añaido con exito'
     }
-    getProducts(){
+    getProducts() {
         return this.#products
     }
-    getProductsById(id){
+    getProductsById(id) {
         const product = this.#products.find(p => p.id == id)
         if (product)
             return product
         else
             return `Not Found`
+    }
+    updateProducts(id, updateObjects) {
+        let message = `EL producto ${id} no existe`
+
+        const index = this.#products.findIndex(p => p.id === id)
+        if (index !== 1) {
+            const { id, ...rest } = updateObjects;
+            this.#products[index] = { ...this.#products[index], ...rest }
+            this.#saveFile()
+            message = 'Producto actualizado con exito!'
+        }
+        return message
+    }
+    deletProduct(id) {
+        let message = `EL producto ${id} no existe`
+        const index = this.#products.findIndex(p => p.id == id)
+        if (index !== -1) {
+            this.#products = this.#products.filter(p => p.id !== id)
+            this.#saveFile()
+            message = 'Producto eliminado con exito!'
+        }
     }
 }
 
--- test.js
+++ test.js
@@ -3,8 +3,17 @@ const Productmanager = require("./ProductManager");
 const producto = new Productmanager()
 
 // console.log(producto.getProductsById(1));
-console.log(producto.addProduct('laptop','dell 16.5"', '100000','www.google.com','19734', '2'));
-console.log(producto.addProduct('laptop','dell 16.5"', '100000','www.google.com','19735', '2'));
+// console.log(producto.addProduct('laptop','dell 16.5"', '100000','www.google.com','19734', '2'));
+// console.log(producto.addProduct('laptop','dell 16.5"', '100000','www.google.com','19735', '2'));
+// console.log(producto.addProduct('laptop','dell 16.5"', '100000','www.google.com','19736', '2'));
 
-console.log(producto.getProducts());
-console.log(producto.getProductsById(3));
+// // console.log(producto.getProducts());
+// // console.log(producto.getProductsById(3));
+// // console.log(producto.deletProduct(3));
+const updateProduct =  {
+    "id": 30,
+    "title": "laptop 2",
+    "price": "50",
+    "stock": "20"
+}
+console.log(producto.updateProducts(3, updateProduct));
